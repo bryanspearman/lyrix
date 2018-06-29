9366eec023b0933476c8e45803c48d968c0a0535
Bryan Spearman <bryanspearman@yahoo.com>
2018-06-29 13:38:10 -0400

    adding in lyrics API functionality

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const YouTube_Search_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+// API End Points
+const YouTube_Search_URL = 'https://www.googleapis.com/youtube/v3/search';
+const Lyrics_URL = 'https://orion.apiseeds.com/api/music/lyric/';
+
+
+
+// API call for lyrics data
+function getLyricsData(artist, track, callback) {
+  const query = {
+    key: `?apikey=oK4BjTgWNvSOZObSdtkDUIT9ERJyR53WYqetVDZvVi4ynNhHAENoybpyN3K5tmQl`,
+  }
+  $.getJSON(`${Lyrics_URL}/${artist}/${track}`, query, callback);
+}
+
+
+//Create the lyrics HTML
+function renderLyrics(result) {
+  return `
+      <h3>${result.track.name}</h3>
+      <h4>By: ${result.artist.name}</h4>
+        <p>${result.track.text}</p>
+        <p class="copyright">${result.copyright.text}</p> 
+      }
+  `;
+}
+
+
+
+//Look over the data and use the render function to organize it on the page
+function displayLyricsData(data) {
+    const results = data.items.map((item, index) => renderVideos(item));
+  $('.vidz').html(results);
+
+}
+
+
+
+// API call for video data
 function getVideoData(searchTerm, callback) {
   const settings = {
     url: YouTube_Search_URL,
     data: {
       q: `${searchTerm} in:name`,
       part: 'snippet',
-      per_page: 2,
+      per_page: 5,
       key: 'AIzaSyCJUNC_Bo_DvZJgUU3P_p4dXVfZhtv1nLI',
     },
     dataType: 'json',
@@ -17,32 +54,25 @@ function getVideoData(searchTerm, callback) {
   $.ajax(settings);
 }
 
-function renderResult(result) {
+
+//Create the video HTML
+function renderVideos(result) {
   return `
-      <div class="searchResult">
-        <div class="row">
-          <div class="col-4">
-           <p><a data-fancybox class="iframe" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}"><img class="videoThumbnail" src="${result.snippet.thumbnails.medium.url}" border="0" alt="${result.snippet.title}"/></a></p>
-          </div>
-        <div class="col-6">
-          <h3>
+          <p><a data-fancybox class="iframe" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}"><img class="videoThumbnail" src="${result.snippet.thumbnails.medium.url}" border="0" alt="${result.snippet.title}"/></a></p>
+           <h3>
             <a data-fancybox class="js-result-title" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a>
           </h3>
-          <p class="postedBy">
-            Posted by: <a href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank" title="author of video"><b>${result.snippet.channelTitle}</b>
-          </p>
           <p>
             <a class="channelLink" title="visit this youtube channel" href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank"><i class="fab fa-youtube"></i> Visit Channel</a>
-          </p>
-       </div>
-      </div>
-    </div>
+          </p>  
   `;
 }
 
+
+//Look over the data and use the render function to organize it on the page
 function displayVideoData(data) {
-    const results = data.items.map((item, index) => renderResult(item));
-  $('.results-container').html(results);
+    const results = data.items.map((item, index) => renderVideos(item));
+  $('.vidz').html(results);
 
 }
 
