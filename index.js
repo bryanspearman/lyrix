b4386839514d8fc3ed3d118bbc682338df7f723e
Bryan Spearman <bryanspearman@yahoo.com>
2018-07-02 16:00:12 -0400

    final updates to wireframes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+'use strict';
 
-// API End Points
 const YouTube_Search_URL = 'https://www.googleapis.com/youtube/v3/search';
 const Lyrics_URL = 'https://orion.apiseeds.com/api/music/lyric';
 
 
 
-// API call for lyrics data
+// Lyrics handling
+
 function getLyricsData(artist, track, callback) {
   const query = {
     apikey:`oK4BjTgWNvSOZObSdtkDUIT9ERJyR53WYqetVDZvVi4ynNhHAENoybpyN3K5tmQl`,
@@ -13,26 +14,26 @@ function getLyricsData(artist, track, callback) {
   $.getJSON(`${Lyrics_URL}/${artist}/${track}`, query, callback);
 }
 
-
-//Create the lyrics HTML
-function renderLyrics(result) {
+function renderLyricsHtml(result) {
   return `
       <h3>${result.track.name}</h3>
-      <h4>By: ${result.artist.name}</h4>
-        <p>${result.track.text}</p>
+      <p class="byName">Lyrics by: ${result.artist.name}</p>
+        <pre>${result.track.text}</pre>
         <p class="copyright">${result.copyright.text}</p> 
   `;
 }
 
-//Look over the data and use the renderLyrics function to organize it on the page
-function displayLyricsData(data) {
-    const results = result.map((item, index) => renderLyrics(item));
+function LyricsDataCallback(data) {
+    const results = renderLyricsHtml(data.result);
   $('.lyrix').html(results);
 }
 
 
 
-// API call for video data
+
+
+//Video handling
+
 function getVideoData(searchTerm, callback) {
   const settings = {
     url: YouTube_Search_URL,
@@ -50,27 +51,33 @@ function getVideoData(searchTerm, callback) {
   $.ajax(settings);
 }
 
-
-//Create the video HTML
-function renderVideos(result) {
+function renderVideoHtml(result) {
   return `
-          <p><a data-fancybox class="iframe" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}"><img class="videoThumbnail" src="${result.snippet.thumbnails.medium.url}" border="0" alt="${result.snippet.title}"/></a></p>
-           <h3>
-            <a data-fancybox class="js-result-title" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a>
-          </h3>
-          <p>
-            <a class="channelLink" title="visit this youtube channel" href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank"><i class="fab fa-youtube"></i> Visit Channel</a>
-          </p>  
+    <p><a data-fancybox class="iframe" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}"><img class="videoThumbnail" src="${result.snippet.thumbnails.high.url}" border="0" alt="${result.snippet.title}"/></a></p>
+     <h3>
+      <a data-fancybox class="js-result-title" title="opens video ${result.snippet.title}" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a>
+    </h3>
+    <p>
+      <a class="channelLink" title="visit this youtube channel" href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank"><i class="fab fa-youtube"></i> Visit Channel</a>
+    </p>  
   `;
 }
 
-
-//Look over the data and use the renderVideos function to organize it on the page
-function displayVideoData(data) {
-    const results = data.items.map((item, index) => renderVideos(item));
+function VideoDataCallback(data) {
+    const results = data.items.map((item, index) => renderVideoHtml(item));
   $('.vidz').html(results);
-
 }
+
+
+
+//a11y tweak to unhide the results area
+
+function toggleHiddenAttr() {
+  $('section').prop('hidden', false);
+}
+
+
+//Launch functionality
 
 function beginSearch() {
   $('.js-search-form').submit(event => {
@@ -81,11 +88,10 @@ function beginSearch() {
     const song = querySong.val();    
     queryArtist.val("");
     querySong.val("");
-    getLyricsData(artist, song, displayLyricsData);
-    getVideoData(song, displayVideoData);
+    getLyricsData(artist, song, LyricsDataCallback);
+    getVideoData(song, VideoDataCallback);
+    toggleHiddenAttr();
   });
 }
-
-
 
 $(beginSearch);
